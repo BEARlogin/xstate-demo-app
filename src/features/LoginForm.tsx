b8d43aec3bc088acfd74d3f103784918246dd5b0
bearlogin <bearlogin@mail.ru>
2022-06-25 15:15:36 +0300

    initial

--- src/features/LoginForm.tsx
+++ src/features/LoginForm.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useEffect, useState} from 'react'
 import {useMachine} from '@xstate/react';
 import loginMachine, {Events, States} from "./login-machine";
 import {FormInput} from "../shared/forms/FormInput";
